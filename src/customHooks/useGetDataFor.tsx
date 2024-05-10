308fc529786a29d8228e80a3afd234a217b9c2e8
Kristoffer Nowén <kristoffer.nowen@learnet.se>
2024-05-10 12:09:43 +0200

    add dynamic table that accepts different lists(must contain id) and functions(id?) for actions on buttons. Test custom hook for fetch instead of httpservice.

---
+++ src/customHooks/useGetDataFor.tsx
@@ -0,0 +1,24 @@
+import React, {useEffect, useState} from "react";
+
+export function useGetDataFor<T>(url: string): [T[], boolean, Error | null] {
+    const [data, setData] = useState<T[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [error, setError] = useState<Error | null>(null);
+
+    useEffect(() => {
+        async  function fetchData() {
+            try {
+                const response = await fetch(url);
+                const data = await response.json();
+                setData(data);
+            } catch (error: unknown) {
+                setError(error as React.SetStateAction<Error | null>);
+            } finally {
+                setLoading(false);
+            }
+        }
+        fetchData();
+    }, [])
+
+    return [data, loading, error];
+}
--- src/pages/ManageExercisesPage.tsx
+++ src/pages/ManageExercisesPage.tsx
@@ -1,8 +1,38 @@
+import LearnerTable, {ColumnName} from "../components/general/table/LearnerTable";
+import {SelectExercise} from "./Home";
+import {type FunctionForLearnerTable} from "../components/general/table/LearnerTable";
+import {urls} from "../utilities/urls";
+import {useGetDataFor} from "../customHooks/useGetDataFor";
+
+const columnNames: ColumnName[] = [{
+    columnName: "Namn",
+    propName: "name"
+}];
+
+const buttonFunctions: FunctionForLearnerTable[] = [
+    {
+        buttonLabel: "säg första",
+        tableFunction: function sayFirst() {
+            console.log("första")
+        }
+    },
+    {
+        buttonLabel: "säg id",
+        tableFunction: function sayId(id?: string){
+            console.log(id)
+        }
+    }
+];
+
 export default function ManageExercisesPage() {
 
-    return (
-        <>
-            <h1>Hantera övningar</h1>
-        </>
-    )
+    const [exercises, loading, error] = useGetDataFor<SelectExercise>(urls.getExercises);
+
+    if(loading) return <div>Hämtar övningar...</div>
+    if(error) return <div>Det blev ett fel: {error.message}</div>
+
+    return <>
+        <h1>Hantera övningar</h1>
+        <LearnerTable data={exercises} columnNames={columnNames} buttonFunctions={buttonFunctions}/>
+    </>
 }
