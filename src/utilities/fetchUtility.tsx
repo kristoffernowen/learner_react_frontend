50fd514eeb1826c4730561e98a7683ee150ab075
kristoffernowen <89974843+kristoffernowen@users.noreply.github.com>
2024-05-15 17:43:23 +0200

    Add delete function to table in manage exercise page (#11)
    * Feature/practice page (#2)
    
    * Add ExerciseForm and ResultBox, exercise can now be done and results received
    
    * Use learnerTable for results on practicePage
    
    * delete ResultBox, minor text adjust learner table reuslt
    
    * fix import for Result type (remove resultbox reference)
    
    * Feature/create exercise (#5)
    
    * add page with components for creating exercise: forms, inputWithLabel, GrayBorderedBox, fetchUtility
    
    * add url to dependencyarray of useGetRecordsFor hook, seems to work correctly at first render locally at least
    
    * add way too much but primarily first css adjustment for small screen, mostly navbar and the gray box showing what will be included plus small fixes.
    customHooks to fetch, loading start as false and set as true then false, instead of true to false
    
    * fix urls back to azure after accidentally committing local ones
    
    * could not set custom hook for getting single record isLoading to false initially, without adjusting more code, so i revert that part. small cleanup
    
    * add delete function in manage exercise page, that will use added delete in fetch utility. Add option for async functions in Learner table. Also clean up url collection a little. I had to add return setData to get records hook, this in turn made me have to add empty slot for setData in Home page.

--- src/utilities/fetchUtility.tsx
+++ src/utilities/fetchUtility.tsx
@@ -23,3 +23,17 @@ export async function post<T, U>(url: string, body: U ) {
     const data = await response.json() as unknown;
     return data as T;
 }
+
+export async function deleteWithFetch(url: string, id: string): Promise<boolean> {
+        try {
+            const response = await fetch(`${url}${id}`, {
+                method: "DELETE",
+                headers: {
+                    'Content-Type': 'application/json',
+                }
+            });
+            return response.ok;
+
+        } catch {}
+    return false;
+}
