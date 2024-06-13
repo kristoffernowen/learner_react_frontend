8eabe5953d4dd3cf63b9836307472cd4951397c0
kristoffernowen <89974843+kristoffernowen@users.noreply.github.com>
2024-06-13 15:24:14 +0200

    Add wakeup call to api and move choose exercise from home. (#19)
    * Feature/practice page (#2)
    
    * Add ExerciseForm and ResultBox, exercise can now be done and results received
    
    * Use learnerTable for results on practicePage
    
    * delete ResultBox, minor text adjust learner table reuslt
    
    * fix import for Result type (remove resultbox reference)
    
    * Feature/create exercise (#5)
    
    * add page with components for creating exercise: forms, inputWithLabel, GrayBorderedBox, fetchUtility
    
    * add url to dependencyarray of useGetRecordsFor hook, seems to work correctly at first render locally at least
    
    * add way too much but primarily first css adjustment for small screen, mostly navbar and the gray box showing what will be included plus small fixes.
    customHooks to fetch, loading start as false and set as true then false, instead of true to false
    
    * fix urls back to azure after accidentally committing local ones
    
    * could not set custom hook for getting single record isLoading to false initially, without adjusting more code, so i revert that part. small cleanup
    
    * add delete function in manage exercise page, that will use added delete in fetch utility. Add option for async functions in Learner table. Also clean up url collection a little. I had to add return setData to get records hook, this in turn made me have to add empty slot for setData in Home page.
    
    * add focus on input fields, using refs or querySelector, also disable button when no name is set and the continue buttons when no facts or factObjects are added. (#12)
    
    * remove missed dead import useEffect in Home
    
    * add staticwebapp.config.json route/* and navigation fallback index html and see if routes work then
    
    * Add a wakeup call to the api in App, and move choose exercise from Home to ChooseExercise. (#18)

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import Home from "./pages/Home";
 import ManageExercisesPage from "./pages/ManageExercisesPage";
 import PracticePage from "./pages/PracticePage";
 import CreateExercisePage from "./pages/CreateExercisePage";
+import ChooseExercise from "./pages/ChooseExercise";
+import {urls} from "./utilities/urls";
+import {useEffect} from "react";
 
 const router = createBrowserRouter([
     {path: "/", element: <RootLayoutPage />, children: [
@@ -12,11 +15,30 @@ const router = createBrowserRouter([
             {path: "/manage-exercises", element: <ManageExercisesPage /> },
             {path: "practice-page/:id", element: <PracticePage />},
             { path: "/new-exercise", element: <CreateExercisePage />},
+            {path: "choose-exercise", element: <ChooseExercise />}
         ]}
 ])
 
+const wakeUpApi = async () => {
+    try{
+        const response = await fetch(urls.getExercises);
+        if(!response.ok){
+            console.error("Api wakeup call. The first is expected to fail.", response.statusText)
+        }
+    } catch (error: unknown) {
+        if(error instanceof Error){
+            console.error("An unexpected error occurred", error.message);
+        } else {
+            console.error("An unexpected error occurred");
+        }
+    }
+};
+
 function App() {
 
+    useEffect(() => {
+        wakeUpApi();
+    }, []);
   return (
     <RouterProvider router={router} />
   )
---
+++ src/pages/ChooseExercise.tsx
@@ -0,0 +1,45 @@
+import LearnerTable, {ColumnName, FunctionForLearnerTable} from "../components/general/table/LearnerTable";
+import {urls} from "../utilities/urls";
+import {useGetRecordsFor} from "../customHooks/useGetRecordsFor";
+import {useNavigate} from "react-router";
+
+export type SelectExercise = {
+    name: string,
+    id: string
+};
+
+const columnNames: ColumnName[] = [{
+    columnName: "Namn",
+    propName: "name"
+}];
+
+export default function ChooseExercise() {
+    const [exercises, , loading, error] = useGetRecordsFor<SelectExercise>(urls.getExercises);
+
+    const navigate = useNavigate();
+
+    const buttonFunctions: FunctionForLearnerTable[] = [
+        {
+            buttonLabel: "Starta övning",
+            tableFunction: function startExercise(id?: string) {
+                navigate(`/practice-page/${id}`)
+            }
+        }
+    ];
+
+    const loadingMessage = <div>Hämtar övningar...</div>;
+    const errorMessage = <div>Det blev ett fel: {error ? error.message : "no error"}</div>;
+
+    return (
+        <>
+            <h1>Välj en övning att göra</h1>
+            {
+                loading ?
+                    loadingMessage:
+                    error ?
+                        errorMessage:
+                        <LearnerTable data={exercises} columnNames={columnNames} buttonFunctions={buttonFunctions}/>
+            }
+        </>
+    )
+}
