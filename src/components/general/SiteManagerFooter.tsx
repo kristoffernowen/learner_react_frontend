34a5a9263802b911d64ca1f9caba2d2a99cbc68a
kristoffernowen <89974843+kristoffernowen@users.noreply.github.com>
2024-07-10 14:05:03 +0200

    Develop (#20)
    * Feature/practice page (#2)
    
    * Add ExerciseForm and ResultBox, exercise can now be done and results received
    
    * Use learnerTable for results on practicePage
    
    * delete ResultBox, minor text adjust learner table reuslt
    
    * fix import for Result type (remove resultbox reference)
    
    * Feature/create exercise (#5)
    
    * add page with components for creating exercise: forms, inputWithLabel, GrayBorderedBox, fetchUtility
    
    * add url to dependencyarray of useGetRecordsFor hook, seems to work correctly at first render locally at least
    
    * add way too much but primarily first css adjustment for small screen, mostly navbar and the gray box showing what will be included plus small fixes.
    customHooks to fetch, loading start as false and set as true then false, instead of true to false
    
    * fix urls back to azure after accidentally committing local ones
    
    * could not set custom hook for getting single record isLoading to false initially, without adjusting more code, so i revert that part. small cleanup
    
    * add delete function in manage exercise page, that will use added delete in fetch utility. Add option for async functions in Learner table. Also clean up url collection a little. I had to add return setData to get records hook, this in turn made me have to add empty slot for setData in Home page.
    
    * add focus on input fields, using refs or querySelector, also disable button when no name is set and the continue buttons when no facts or factObjects are added. (#12)
    
    * remove missed dead import useEffect in Home
    
    * add staticwebapp.config.json route/* and navigation fallback index html and see if routes work then
    
    * Add a wakeup call to the api in App, and move choose exercise from Home to ChooseExercise. (#18)
    
    * Add footer with SiteManager info

---
+++ src/components/general/SiteManagerFooter.tsx
@@ -0,0 +1,9 @@
+import styles from "./SiteManagerFooter.module.css"
+
+export default function SiteManagerFooter() {
+    return (
+        <footer className={styles.footer}>
+            <p>Sidansvarig: Kristoffer Nowén</p>
+        </footer>
+    );
+}
