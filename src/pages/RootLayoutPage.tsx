f6473a40ada8d8a4f280f83dbfc77e22dae43919
Kristoffer Nowén <kristoffer.nowen@learnet.se>
2024-07-10 13:58:18 +0200

    Add footer with SiteManager info

--- src/pages/RootLayoutPage.tsx
+++ src/pages/RootLayoutPage.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom"
 import NavMenu from "../components/NavMenu";
+import SiteManagerFooter from "../components/general/SiteManagerFooter";
 
 export default function RootLayoutPage() {
     return <div>
         <NavMenu />
             <Outlet />
+        <SiteManagerFooter />
     </div>;
 }
